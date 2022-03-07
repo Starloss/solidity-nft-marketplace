833b1b6cb378d2014becfd587e668376473dfee1
Diego Rangel <starloss09@gmail.com>
2022-03-07 00:37:19 -0400

    Added function to buy with ETH Added chainlink library Added functions for set feeds Added functions for get prices of ETH, DAI and LINK

--- contracts/NFTs_Marketplace.sol
+++ contracts/NFTs_Marketplace.sol
@@ -2,35 +2,48 @@
 pragma solidity >=0.8.0 <0.9.0;
 
 import "@openzeppelin/contracts-upgradeable/access/AccessControlUpgradeable.sol";
+import '@openzeppelin/contracts-upgradeable/security/ReentrancyGuardUpgradeable.sol';
 import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
+import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
 
-contract NFTsMarketplace is AccessControlUpgradeable {
+contract NFTsMarketplace is AccessControlUpgradeable, ReentrancyGuardUpgradeable {
 
     /// VARIABLES
     uint adminFee = 1;
+    uint orderCount = 0;
+
+    AggregatorV3Interface internal ETHFeed;
+    AggregatorV3Interface internal DAIFeed;
+    AggregatorV3Interface internal LINKFeed;
 
     /**
      *  @notice Bytes32 used for roles in the Dapp
      */
     bytes32 public constant ADMIN_ROLE = keccak256("ADMIN_ROLE");
 
-    struct Offer {
+    struct Order {
         address tokenAddress;
+        address payable seller;
         uint tokenID;
         uint tokenAmount;
         uint deadline;
         uint price;
+        uint orderID;
+        OrderState state;
     }
     
-    mapping(address => Offer[]) offersByAddress;
+    mapping(uint => Order) ordersByID;
 
     /// STATES
+
+    enum OrderState {OPEN, DONE, CANCELED, ERROR_APPROVED, ERROR_AMOUNT, TIME_ENDED}
 
     /// EVENTS
     /**
      *  @notice Event emitted when an Sell Order is created
      */
     event SellOrderCreated(
+        uint _orderID,
         address _seller,
         address _tokenAddress,
         uint _tokenID,
@@ -47,10 +60,16 @@ contract NFTsMarketplace is AccessControlUpgradeable {
      */
     function initialize() public initializer {
         __AccessControl_init();
+        __ReentrancyGuard_init();
+
         _grantRole(ADMIN_ROLE, msg.sender);
+
+        ETHFeed = AggregatorV3Interface(0x8A753747A1Fa494EC906cE90E9f37563A8AF630e);
+        DAIFeed = AggregatorV3Interface(0x2bA49Aaa16E6afD2a993473cfB70Fa8559B523cF);
+        LINKFeed = AggregatorV3Interface(0xd8bD0a1cB028a31AA859A21A3758685a95dE4623);
     }
 
-    function createNewOffer(
+    function createNewOrder(
         address _tokenAddress,
         uint _tokenID,
         uint _tokenAmount,
@@ -62,15 +81,86 @@ contract NFTsMarketplace is AccessControlUpgradeable {
         require(token.isApprovedForAll(msg.sender, address(this)), "This contract is not allowed to transfer sender's tokens");
         require(token.balanceOf(msg.sender, _tokenID) >= _tokenAmount, "The user has not enough tokens");
 
-        Offer memory newOffer = Offer(_tokenAddress, _tokenID, _tokenAmount, _deadline, _price);
-        offersByAddress[msg.sender].push(newOffer);
+        orderCount++;
 
-        emit SellOrderCreated(msg.sender, _tokenAddress, _tokenID, _tokenAmount, _deadline, _price);
+        Order memory newOrder = Order(_tokenAddress, payable(msg.sender), _tokenID, _tokenAmount, _deadline, _price, orderCount, OrderState.OPEN);
+        ordersByID[orderCount] = newOrder;
+
+        emit SellOrderCreated(orderCount, msg.sender, _tokenAddress, _tokenID, _tokenAmount, _deadline, _price);
+    }
+
+    function buyWithETH(uint _orderID) payable public {
+        uint weiCost = (ordersByID[_orderID].price * 10 ** 36) / uint(getETHPrice() * 10 ** 10);
+
+        require(ordersByID[_orderID].state == OrderState.OPEN, "The order is not available");
+        require(msg.value >= weiCost, "Not enough ");
+
+        if (ordersByID[_orderID].deadline <= block.timestamp) {
+            ordersByID[_orderID].state = OrderState.TIME_ENDED;
+            revert("The order has reached his deadline");
+        }
+
+        ERC1155 token = ERC1155(ordersByID[_orderID].tokenAddress);
+
+        if (token.isApprovedForAll(ordersByID[_orderID].seller, address(this))) {
+            ordersByID[_orderID].state = OrderState.ERROR_APPROVED;
+            revert("The seller has revoked access to their tokens to this contract");
+        }
+
+        if (token.balanceOf(ordersByID[_orderID].seller, ordersByID[_orderID].tokenID) >= ordersByID[_orderID].tokenAmount) {
+            ordersByID[_orderID].state = OrderState.ERROR_AMOUNT;
+            revert("The seller doesn't have enough tokens");
+        }
+
+        (bool success, ) = msg.sender.call{value: msg.value - weiCost}("");
+        require(success);
+
+        ordersByID[_orderID].state = OrderState.DONE;
+
+        (success, ) = ordersByID[_orderID].seller.call{value: (weiCost * 100) / 99}("");
+        require(success);
+
+        token.safeTransferFrom(ordersByID[_orderID].seller, msg.sender, ordersByID[_orderID].tokenID, ordersByID[_orderID].tokenAmount, "");
+    }
+
+    function buyWithDai() public {
+        
+    }
+    
+    function buyWithLink() public {
+        
     }
 
     function setAdminFee(uint _adminFee) public onlyRole(ADMIN_ROLE) {
         require(_adminFee >= 0 && _adminFee <= 100, "Wrong fee!");
 
         adminFee = _adminFee;
+    }
+
+    function setETHFedd(address _address) public onlyRole(ADMIN_ROLE) {
+        ETHFeed = AggregatorV3Interface(_address);
+    }
+
+    function setDAIFedd(address _address) public onlyRole(ADMIN_ROLE) {
+        DAIFeed = AggregatorV3Interface(_address);
+    }
+
+    function setLINKFedd(address _address) public onlyRole(ADMIN_ROLE) {
+        LINKFeed = AggregatorV3Interface(_address);
+    }
+
+    function getETHPrice() private view returns (int) {
+        ( , int price, , , ) = ETHFeed.latestRoundData();
+        return price;
+    }
+
+    function getDAIPrice() private view returns (int) {
+        ( , int price, , , ) = DAIFeed.latestRoundData();
+        return price;
+    }
+
+    function getLINKPrice() private view returns (int) {
+        ( , int price, , , ) = LINKFeed.latestRoundData();
+        return price;
     }
 }
